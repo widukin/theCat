c5c88633e8075a2bcd291de8f90fc3ebb4843861
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2020-12-10 19:18:46 +0100

    create Cat object with properties

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 ; (function (window, document) {
   'use strict';
+  /* declaration section */
 
-  /* declare variables and functions here */
+  class Cat {
+    constructor (tiredness, hunger, loneliness, happiness) {
+      this._tiredness = tiredness;
+      this._hunger = hunger;
+      this._loneliness = loneliness;
+      this._happiness = happiness;
+    }
+  }
+
 
   window.onload = function () {
 

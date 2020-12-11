10fe058d47686291982d25ea11d92d376672bac5
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2020-12-11 11:49:45 +0100

    cats Status

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,7 @@
 
 class Cat {
   constructor (tiredness = 0, hunger = 0, loneliness = 0, happiness = 0) {
+    this._name = 'Paws'
     this._tiredness = tiredness;
     this._hunger = hunger;
     this._loneliness = loneliness;
@@ -14,42 +15,105 @@ class Cat {
   }
 
   sleep () {
-    tiredness++;
+    this._tiredness++;
   }
 
   feed () {
-    hunger--;
+    this._hunger--;
   }
 
   pet () {
-    loneliness--;
+    this._loneliness--;
   }
 
   play () {
-    happiness++;
+    this._happiness++;
   }
 
-  setCatsStatus () {
+  setStateOfTiredness () {
+    let stateOfTiredness;
 
+    if (this._tiredness < 0) {
+      return stateOfTiredness = 'tired';
+    } else if (this._tiredness === 0) {
+      return stateOfTiredness = 'awake';
+    } else if (this._tiredness > 0) {
+      return stateOfTiredness = 'excited';
+    }
+  }
+
+  setStateOfHunger () {
+    let stateOfHunger;
+
+    if (this._hunger < 0) {
+      return stateOfHunger = 'stuffed';
+    } else if (this._hunger === 0) {
+      return stateOfHunger = 'full';
+    } else if (this._hunger > 0) {
+      return stateOfHunger = 'hungry';
+    }
+  }
+
+  setStateOfLoneliness () {
+    let stateOfLoneliness;
+
+    if (this._loneliness < 0) {
+      return stateOfLoneliness = 'accompanied';
+    } else if (this._loneliness === 0) {
+      return stateOfLoneliness = 'alone';
+    } else if (this._loneliness > 0) {
+      return stateOfLoneliness = 'lonely';
+    }
+  }
+
+  setStateOfHappiness () {
+    let stateOfHappiness;
+      if (this._happiness < 0) {
+        console.log(this._happiness + ' depressed');
+      return stateOfHappiness = 'depressed';
+    } else if (this._happiness === 0) {
+      console.log(this._happiness + ' ok');
+      return stateOfHappiness = 'ok';
+    } else if (this._happiness > 0) {
+      console.log(this._happiness + ' happy');
+      return stateOfHappiness = 'happy';
+    }
   }
 
   getCatsStatus () {
 
-    return 'i am fine';
+    let stateOfTiredness = this.setStateOfTiredness();
+    let stateOfHunger = this.setStateOfHunger();
+    let stateOfLoneliness = this.setStateOfLoneliness();
+    let stateOfHappiness = this.setStateOfHappiness();
+
+    if (this._tiredness < 0 && this._hunger < 0 && this._loneliness > 0 && this._happiness < 0) {
+      return `${this._name} is really ${stateOfHunger} and so ${stateOfTiredness} at the moment. He is also feeling ${stateOfHappiness} because he is ${stateOfLoneliness}. Oh boy, he is very suicidal, right now! Please care about ${this._name}.`;
+    } else if (this._tiredness > 0 && this._hunger === 0 && this._loneliness < 0 && this._happiness > 0) {
+      return `Dont pet ${this._name}!`;
+    } else {
+      return `${this._name} is really ${stateOfHunger} and ${stateOfTiredness} right now. He is feeling ${stateOfHappiness} because he is ${stateOfLoneliness}.`;
+    }
   }
 }
+
+/* 
+- Bonus: Make the functions take arguments that increase or decrease arbitrary amounts
+*/
 
   
   //ES6
   const getOutput = () => {
     const outputContainer = document.getElementById('output');
-    const catPaws = new Cat();
-    const outputText = catPaws.getCatsStatus();
-    outputContainer.innerHTML = outputText + 'Hallo';
+    /* instanciate Cat */
+    const cat = new Cat();
+    /* get Cats Status */
+    const outputText = cat.getCatsStatus();
+    /* Print the Cats Status into the Output Container of the index.html */
+    outputContainer.innerHTML = outputText;
   }
 
   const bindClickHandler = () => {
-    console.log('bindClickHandler');
     const button = document.getElementsByTagName('button')[0];
     button.addEventListener('click', getOutput);
   }
